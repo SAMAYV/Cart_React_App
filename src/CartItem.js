81a6c7d3f3670e2d5cbfc57ec54dd5709fdd5a7a
SAMAYV <samay@iitg.ac.in>
2021-06-14 11:33:24 +0530

    component edited

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,30 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ""
+        }
+    }
     render () {
+        const {price, title, qty} = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize: 25}}>Phone</div>
-                    <div style={{color: '#777'}}>Rs 999</div>
-                    <div style={{color: '#777'}}>Qty 1</div>
+                    <div style={{fontSize: 25}}>{title}</div>
+                    <div style={{color: '#777'}}>Rs {price}</div>
+                    <div style={{color: '#777'}}>Qty {qty}</div>
                     <div className="cart-item-actions">
-
+                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/png/512/3303/3303893.png" />
+                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/png/512/2740/2740679.png" />
+                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/png/512/3161/3161358.png" />
                     </div>
                 </div>
             </div>
