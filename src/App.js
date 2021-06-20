8a9145437442b816e5fe6d58865a2e0a24feff44
SAMAYV <samay@iitg.ac.in>
2021-06-20 13:25:18 +0530

    delete prd added for firebase

--- src/App.js
+++ src/App.js
@@ -32,6 +32,11 @@ class App extends React.Component {
       	const index = products.indexOf(product);
 		const docRef = this.db.collection('products').doc(products[index].id)
 		
+		// products[index].qty += 1;
+		// this.setState({
+			// products: products
+		// })
+
 		docRef	
 			.update({
 				qty: products[index].qty + 1
@@ -50,19 +55,40 @@ class App extends React.Component {
 		if(products[index].qty === 0){
 			return;
 		}
-		products[index].qty -= 1;
+		
+		// products[index].qty -= 1;
+		// this.setState({
+			// products: products
+		// })
 
-		this.setState({
-			products: products
-		})
+		const docRef = this.db.collection('products').doc(products[index].id)
+		docRef	
+			.update({
+				qty: products[index].qty - 1
+			})
+			.then(() => {
+				console.log('Updated successfully');
+			})
+			.catch((err) => {
+				console.log('Error: ', err);
+			})
 	}
 	handleDeleteProduct = (id) => {
-		const {products} = this.state;
-		const items = products.filter((item) => item.id !== id);
+		// const {products} = this.state;
+		// const items = products.filter((item) => item.id !== id);
+		// this.setState({
+		// 	products: items
+		// })
 
-		this.setState({
-			products: items
-		})
+		const docRef = this.db.collection('products').doc(id);
+		docRef
+			.delete()
+			.then(() => {
+				console.log('Deleted successfully');
+			})
+			.catch((err) => {
+				console.log('Error: ', err);
+			})
 	}
 	getCartCount = () => {
 		const {products} = this.state;
