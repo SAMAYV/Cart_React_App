f4dc55bd1ac9d6e6b28b25e10214ccf29a5f4e49
SAMAYV <samay@iitg.ac.in>
2021-07-06 22:27:51 +0530

    indended properly

--- src/App.js
+++ src/App.js
@@ -1,148 +1,149 @@
-import React from 'react'
-import Cart from './Cart';
-import Navbar from './Navbar';
-import firebase from 'firebase/app';
+import React from "react";
+import Cart from "./Cart";
+import Navbar from "./Navbar";
+import firebase from "firebase/app";
 
 class App extends React.Component {
-  	constructor () {
-		super();
-		this.state = {
-			products: [],
-			loading: true
-    	}
-		this.db = firebase.firestore();
-  	}
-	componentDidMount() {
-		this.db
-			.collection('products')
-			// .where('price', '>', 799)
-			.orderBy('price', 'desc')
-			.onSnapshot((snapshot) => {
-				const products = snapshot.docs.map((doc) => {
-					const data = doc.data();
-					data['id'] = doc.id;
-					return data;
-				});
-				this.setState({
-					products,
-					loading: false
-				});
-			})	
-	}
-  	handleIncreaseQuantity = (product) => {
-      	const {products} = this.state;
-      	const index = products.indexOf(product);
-		const docRef = this.db.collection('products').doc(products[index].id)
-		
-		// products[index].qty += 1;
-		// this.setState({
-			// products: products
-		// })
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+      loading: true,
+    };
+    this.db = firebase.firestore();
+  }
+  componentDidMount() {
+    this.db
+      .collection("products")
+      // .where('price', '>', 799)
+      .orderBy("price", "desc")
+      .onSnapshot((snapshot) => {
+        const products = snapshot.docs.map((doc) => {
+          const data = doc.data();
+          data["id"] = doc.id;
+          return data;
+        });
+        this.setState({
+          products,
+          loading: false,
+        });
+      });
+  }
+  handleIncreaseQuantity = (product) => {
+    const { products } = this.state;
+    const index = products.indexOf(product);
+    const docRef = this.db.collection("products").doc(products[index].id);
 
-		docRef	
-			.update({
-				qty: products[index].qty + 1
-			})
-			.then(() => {
-				console.log('Updated successfully');
-			})
-			.catch((err) => {
-				console.log('Error: ', err);
-			})
-	}
-	handleDecreaseQuantity = (product) => {
-		const {products} = this.state;
-		const index = products.indexOf(product);
+    // products[index].qty += 1;
+    // this.setState({
+    // products: products
+    // })
 
-		if(products[index].qty === 0){
-			return;
-		}
+    docRef
+      .update({
+        qty: products[index].qty + 1,
+      })
+      .then(() => {
+        console.log("Updated successfully");
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  };
+  handleDecreaseQuantity = (product) => {
+    const { products } = this.state;
+    const index = products.indexOf(product);
 
-		// products[index].qty -= 1;
-		// this.setState({
-			// products: products
-		// })
+    if (products[index].qty === 0) {
+      return;
+    }
 
-		const docRef = this.db.collection('products').doc(products[index].id)
-		docRef	
-			.update({
-				qty: products[index].qty - 1
-			})
-			.then(() => {
-				console.log('Updated successfully');
-			})
-			.catch((err) => {
-				console.log('Error: ', err);
-			})
-	}
-	handleDeleteProduct = (id) => {
-		// const {products} = this.state;
-		// const items = products.filter((item) => item.id !== id);
-		// this.setState({
-		// 	products: items
-		// })
+    // products[index].qty -= 1;
+    // this.setState({
+    // products: products
+    // })
 
-		const docRef = this.db.collection('products').doc(id);
-		docRef
-			.delete()
-			.then(() => {
-				console.log('Deleted successfully');
-			})
-			.catch((err) => {
-				console.log('Error: ', err);
-			})
-	}
-	getCartCount = () => {
-		const {products} = this.state;
-		let count = 0;
-		products.forEach((product) => {
-		count += product.qty;
-		})
-		console.log(count);
-		return count;
-	}
-	getCartTotal = () => {
-		const {products} = this.state;
-		let cartTotal = 0;
-		products.map((product) => {
-			cartTotal += product.qty*product.price;
-			return '';
-		})
-		return cartTotal;
-	}
-	addProduct = () => {
-		this.db
-			.collection('products')
-			.add({
-				img: '',
-				price: 900,
-				qty: 2,
-				title: 'Washing Machine'
-			})
-			.then((docRef) => {
+    const docRef = this.db.collection("products").doc(products[index].id);
+    docRef
+      .update({
+        qty: products[index].qty - 1,
+      })
+      .then(() => {
+        console.log("Updated successfully");
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  };
+  handleDeleteProduct = (id) => {
+    // const {products} = this.state;
+    // const items = products.filter((item) => item.id !== id);
+    // this.setState({
+    // 	products: items
+    // })
 
-			})
-			.catch((err) => {
-				console.log('Error: ', err);
-			})
-	}
-	render () {
-		const {products, loading} = this.state;
-		return (
-			<div className="App">
-				<Navbar count={this.getCartCount()}/>
-				{/* <button onClick={this.addProduct} style={{padding: 20, fontSize: 20}}>Add a Product</button> */}
-				<Cart 
-					onIncreaseQuantity={this.handleIncreaseQuantity}
-					onDecreaseQuantity={this.handleDecreaseQuantity}
-					onDeleteProduct={this.handleDeleteProduct}
-					products={products}
-				/>
-				{loading && <h1>Loading Products ...</h1>}
-				<div style={{padding:10, fontSize:20}}> TOTAL: {this.getCartTotal()} </div>
-			</div>
-		);
-	}
+    const docRef = this.db.collection("products").doc(id);
+    docRef
+      .delete()
+      .then(() => {
+        console.log("Deleted successfully");
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  };
+  getCartCount = () => {
+    const { products } = this.state;
+    let count = 0;
+    products.forEach((product) => {
+      count += product.qty;
+    });
+    console.log(count);
+    return count;
+  };
+  getCartTotal = () => {
+    const { products } = this.state;
+    let cartTotal = 0;
+    products.map((product) => {
+      cartTotal += product.qty * product.price;
+      return "";
+    });
+    return cartTotal;
+  };
+  addProduct = () => {
+    this.db
+      .collection("products")
+      .add({
+        img: "",
+        price: 900,
+        qty: 2,
+        title: "Washing Machine",
+      })
+      .then((docRef) => {})
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  };
+  render() {
+    const { products, loading } = this.state;
+    return (
+      <div className="App">
+        <Navbar count={this.getCartCount()} />
+        {/* <button onClick={this.addProduct} style={{padding: 20, fontSize: 20}}>Add a Product</button> */}
+        <Cart
+          onIncreaseQuantity={this.handleIncreaseQuantity}
+          onDecreaseQuantity={this.handleDecreaseQuantity}
+          onDeleteProduct={this.handleDeleteProduct}
+          products={products}
+        />
+        {loading && <h1>Loading Products ...</h1>}
+        <div style={{ padding: 10, fontSize: 20 }}>
+          {" "}
+          TOTAL: {this.getCartTotal()}{" "}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -12,15 +12,14 @@ const firebaseConfig = {
   projectId: "cart-91e96",
   storageBucket: "cart-91e96.appspot.com",
   messagingSenderId: "438674867976",
-  appId: "1:438674867976:web:b25cc9f1b17de840760ebd"
+  appId: "1:438674867976:web:b25cc9f1b17de840760ebd",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
